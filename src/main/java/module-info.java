11f772b431d96d35bda285d5431510524d1ab01a
Spynacz <63506536+Spynacz@users.noreply.github.com>
2023-05-31 17:14:39 +0200

    MySQL -> SQLite

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,11 @@
-module org.example {
+module org.forum {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
     opens org.forum to javafx.fxml;
     exports org.forum;
+    exports org.forum.controllers;
+    opens org.forum.controllers to javafx.fxml;
 }

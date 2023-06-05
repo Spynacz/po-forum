2b011a4891a947a2499268190fb746ffc80a00cd
Rafal Ciolek <rafal.ciolek12@gmail.com>
2023-06-06 00:11:22 +0200

    early view of main window

--- src/main/java/org/forum/controllers/MainWindow.java
+++ src/main/java/org/forum/controllers/MainWindow.java
@@ -1,6 +1,31 @@
 package org.forum.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 public class MainWindow {
+
+    @FXML
+    private VBox ThreadsContainer;
+
+    @FXML
+    private Label login;
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    void KeyPressedCheckIfEnterPressed(KeyEvent event) {
+
+    }
+
+    @FXML
+    void SearchFor(MouseEvent event) {
+
+    }
 
 }

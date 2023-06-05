208fd97358ca7ccd6451a6a450fa296ca696b312
Rafal Ciolek <rafal.ciolek12@gmail.com>
2023-06-06 00:36:06 +0200

    added first version of ThreadTab

---
+++ src/main/java/org/forum/controllers/ThreadTab.java
@@ -0,0 +1,18 @@
+package org.forum.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class ThreadTab {
+
+    @FXML
+    private VBox ThreadBodyContainer;
+
+    @FXML
+    private Label login;
+
+    @FXML
+    private Label temat;
+
+}

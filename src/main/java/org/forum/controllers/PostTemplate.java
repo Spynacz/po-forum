d5e2ab35b75d20be67b356d7b172f1300a5e8483
Rafal Ciolek <rafal.ciolek12@gmail.com>
2023-06-07 00:27:05 +0200

    Added PostTempalte

---
+++ src/main/java/org/forum/controllers/PostTemplate.java
@@ -0,0 +1,39 @@
+package org.forum.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+
+public class PostTemplate {
+
+    @FXML
+    private Label autorField;
+
+    @FXML
+    private Label dateField;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button deleteButton1;
+
+    @FXML
+    private TextArea postField;
+
+    @FXML
+    private Label ranKfield;
+
+    @FXML
+    void deleteThread(MouseEvent event) {
+
+    }
+
+    @FXML
+    void editPost(MouseEvent event) {
+
+    }
+
+}

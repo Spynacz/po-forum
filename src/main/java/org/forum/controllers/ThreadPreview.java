bfb161855ad519106699af48ae041d92ae800d7d
Rafal Ciolek <rafal.ciolek12@gmail.com>
2023-06-06 23:26:04 +0200

    added first version of ThreadPrieview

---
+++ src/main/java/org/forum/controllers/ThreadPreview.java
@@ -0,0 +1,30 @@
+package org.forum.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class ThreadPreview {
+
+    @FXML
+    private Label autorField;
+
+    @FXML
+    private Label dataField;
+
+    @FXML
+    private Label date;
+
+    @FXML
+    private Label date1;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    void deleteThread(MouseEvent event) {
+
+    }
+
+}

efe2145b810d02f3bd01f6ebaf305ebae3f561f0
Rafal Ciolek <rafal.ciolek12@gmail.com>
2023-06-10 22:10:19 +0200

    added callback interface

--- src/main/java/org/forum/controllers/utils/CallBack.java
+++ src/main/java/org/forum/controllers/utils/CallBack.java
@@ -4,7 +4,5 @@ import org.forum.controllers.PostTemplate;
 import org.forum.controllers.ThreadPreview;
 
 public interface CallBack {
-    void invoked(PostTemplate source);
-    void invoked(ThreadPreview source);
     void invoked(Object source);
 }

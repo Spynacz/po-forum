d0c03318841e4aed22d77edf7ee26584c69c4af6
Rafal Ciolek <rafal.ciolek12@gmail.com>
2023-06-10 15:22:48 +0200

    Login background added

--- src/main/java/org/forum/Main.java
+++ src/main/java/org/forum/Main.java
@@ -2,17 +2,10 @@ package org.forum;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.forum.services.UserService;
-import org.forum.dao.RankDAOImpl;
-import org.forum.dao.UserDAOImpl;
-import org.forum.dao.UserRankDAOImpl;
 
 import java.io.IOException;
-import java.sql.SQLDataException;
-import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * JavaFX App
@@ -23,17 +16,19 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("fxml/LogInWindow"), 640, 480);
+        scene = new Scene(loadFXML("fxml/LogInWindow").load(), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+    public static FXMLLoader setRoot(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = loadFXML(fxml);
+        scene.setRoot(fxmlLoader.load());
+        return fxmlLoader;
     }
-    private static Parent loadFXML(String fxml) throws IOException {
+    private static FXMLLoader loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+        return fxmlLoader;
     }
 
     public static void main(String[] args) {
--- src/main/java/org/forum/controllers/LogInWindow.java
+++ src/main/java/org/forum/controllers/LogInWindow.java
@@ -2,17 +2,21 @@ package org.forum.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import org.forum.Main;
 import org.forum.User;
+import org.forum.dao.ThreadDAO;
 import org.forum.dao.UserDAO;
 import org.forum.dao.UserDAOImpl;
 
 import java.io.IOException;
 
 public class LogInWindow {
+
+
 
 
     private UserDAO userTable;
@@ -29,20 +33,26 @@ public class LogInWindow {
     private PasswordField passwordText;
     @FXML
     void logIn(ActionEvent event) throws IOException {
-        User user = null;
+
         try
         {
+            User user = null;
         user = userTable.getByUsername(logInText.getText());
+            if(user != null && user.getPassword().equals(passwordText.getText()))
+            {
+                FXMLLoader fxmlLoader = Main.setRoot("fxml/MainWindow");
+                MainWindow mainWindow = fxmlLoader.getController();
+                mainWindow.initilizeController(user);
+            }
+            else {
+                throw new RuntimeException("Invalid user or login");
+            }
         }
         catch (RuntimeException e)
         {
         infoLabel.setVisible(true);
         }
-        if(user != null && user.getPassword().equals(passwordText.getText()))
-        {
-            Main.setRoot("fxml/MainWindow");
-            //TODO: Main
-        }
+
 
 
     }
